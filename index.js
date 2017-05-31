0dae0904456fb7db73282e7cc786f6b005e4ecb2
Renato Ricardi <renato.ricardi@hotmail.com>
2017-05-31 14:01:32 -0300

    Improving identation

--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const graphqlHTTP = require('express-graphql')
 const express = require('express')
 const users = require('./schema')
 const app = express()
-app.use('/user', graphqlHTTP({schema:users, pretty: true,graphiql: true}))
+
+app.use('/user', graphqlHTTP({schema:users, pretty: true, graphiql: true}))
+
 app.listen(5000, function () {
   console.log('Server on.')
 })
